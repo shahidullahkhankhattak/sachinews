e0c92f6e872c4adaca0f4d6c662c5324a002e580
Karma <mlive_SKhan2@freedommobile.ca>
2020-04-29 12:45:12 +0500

    added card and loader components

---
+++ src/components/Cards/NewsCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-card class="news-card" flat bordered>
+    <q-card-section :horizontal="$q.screen.gt.xs" :vertical="$q.screen.lt.xs">
+      <q-card-section class="q-pt-xs col-8">
+        <div class="text-overline">{{ news.source }} - {{ news.category }}</div>
+        <div class="text-h5 q-mt-sm q-mb-xs">
+          {{ news.title }}
+        </div>
+        <div class="text-subtitle2"><time>1 hour ago</time></div>
+        <q-space />
+        <div class="text-caption text-grey-8 q-mt-sm">
+          {{ news.description }}
+        </div>
+      </q-card-section>
+
+      <q-card-section class="col-4 flex flex-right">
+        <q-img class="rounded-borders" :src="news.media" height="100%" />
+      </q-card-section>
+    </q-card-section>
+
+    <q-card-actions>
+      <q-btn flat round color="red" icon="favorite_border" />
+      <q-btn flat round color="teal" icon="bookmark" />
+      <q-btn flat round color="primary" icon="share" />
+    </q-card-actions>
+  </q-card>
+</template>
+<script>
+export default {
+  props: ['story'],
+  computed: {
+    news() {
+      const {
+        title, description, media, source: [{ name: source, color }], category: [{ name: category }], slug,
+      } = this.story || { source: [{}], category: [{}] };
+      return {
+        source,
+        category,
+        slug,
+        title,
+        description,
+        media,
+        color,
+      };
+    },
+  },
+};
+</script>
--- src/pages/TopStories/Index.vue
+++ src/pages/TopStories/Index.vue
@@ -8,50 +8,10 @@
               <!-- News cards [START] -->
               <div
                 class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4"
-                v-for="{
-                  _id,
-                  source: { name: sourceName, color },
-                  category: { name: categoryName },
-                  title,
-                  description,
-                  media
-                } in topStories.stories"
-                :key="_id"
+                v-for="story in topStories.stories"
+                :key="story._id"
               >
-                <q-card class="news-card" flat bordered>
-                  <q-card-section
-                    :horizontal="$q.screen.gt.xs"
-                    :vertical="$q.screen.lt.xs"
-                  >
-                    <q-card-section class="q-pt-xs col-8">
-                      <div class="text-overline">
-                        {{ sourceName }} - {{ categoryName }}
-                      </div>
-                      <div class="text-h5 q-mt-sm q-mb-xs">
-                        {{ title }}
-                      </div>
-                      <div class="text-subtitle2"><time>1 hour ago</time></div>
-                      <q-space />
-                      <div class="text-caption text-grey-8 q-mt-sm">
-                        {{ description }}
-                      </div>
-                    </q-card-section>
-
-                    <q-card-section class="col-4 flex flex-right">
-                      <q-img
-                        class="rounded-borders"
-                        :src="media"
-                        height="100%"
-                      />
-                    </q-card-section>
-                  </q-card-section>
-
-                  <q-card-actions>
-                    <q-btn flat round color="red" icon="favorite_border" />
-                    <q-btn flat round color="teal" icon="bookmark" />
-                    <q-btn flat round color="primary" icon="share" />
-                  </q-card-actions>
-                </q-card>
+                <NewsCard :story="story" />
               </div>
               <!-- News cards [END] -->
 
@@ -61,57 +21,7 @@
                 v-for="num in (loading && perPage || 0) "
                 :key="num"
               >
-                <q-card
-                  class="news-card"
-                  flat
-                  bordered
-                >
-                  <q-card-section
-                    :horizontal="$q.screen.gt.xs"
-                    :vertical="$q.screen.lt.xs"
-                  >
-                    <q-card-section class="q-pt-xs col-8">
-                      <q-skeleton width="40%" type="text" />
-                      <q-skeleton
-                        animation="blink"
-                        class="text-h5 q-mt-sm q-mb-xs"
-                      />
-                      <q-skeleton animation="fade" width="100px" type="text" />
-                      <q-skeleton class="q-mt-md" type="text" />
-                      <q-skeleton type="text" />
-                    </q-card-section>
-
-                    <q-card-section class="col-4 flex flex-right">
-                      <q-skeleton
-                        animation="blink"
-                        type="rect"
-                        height="150px"
-                        width="100%"
-                      />
-                    </q-card-section>
-                  </q-card-section>
-
-                  <q-card-actions>
-                    <q-skeleton
-                      class="q-mx-sm q-mb-sm"
-                      type="QBtn"
-                      width="35px"
-                      height="30px"
-                    />
-                    <q-skeleton
-                      class="q-mx-sm q-mb-sm"
-                      type="QBtn"
-                      width="35px"
-                      height="30px"
-                    />
-                    <q-skeleton
-                      class="q-mx-sm q-mb-sm"
-                      type="QBtn"
-                      width="35px"
-                      height="30px"
-                    />
-                  </q-card-actions>
-                </q-card>
+                <NewsLoader />
               </div>
               <!-- Loaders section [END}-->
             </div>
@@ -125,6 +35,8 @@
 <script>
 import { config } from '../../config';
 import { getters, actions, handlePrefetch } from './handleStore';
+import NewsCard from '../../components/Cards/NewsCard';
+import NewsLoader from '../../components/Loaders/NewsLoader';
 
 const {
   app: { logo },
@@ -132,6 +44,7 @@ const {
 
 export default {
   name: 'TopStories',
+  components: { NewsCard, NewsLoader },
   data: () => ({
     logo,
   }),
