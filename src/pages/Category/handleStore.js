06c284219f611ea50643518b6d72a95dde3df860
Karma <mlive_SKhan2@freedommobile.ca>
2020-05-07 00:14:45 +0500

    added 404 to source and categories

--- src/pages/Category/handleStore.js
+++ src/pages/Category/handleStore.js
@@ -1,6 +1,8 @@
 import { mapGetters, mapActions } from 'vuex';
 import { Getters, Actions } from '../../store/Stories/constants';
+import { Getters as CategoryGetters } from '../../store/Categories/constants';
 
+// getters
 const {
   STORIES,
   LOADING,
@@ -8,12 +10,18 @@ const {
   TOTAL,
 } = Getters;
 const {
+  LIST: CATEGORY_LIST,
+} = CategoryGetters;
+
+// actions
+const {
   FETCH_STORIES,
 } = Actions;
 
 export const getters = {
   ...mapGetters({
     stories: STORIES,
+    categories: CATEGORY_LIST,
     loading: LOADING,
     perPage: PER_PAGE,
     total: TOTAL,
@@ -26,10 +34,13 @@ export const actions = {
   }),
 };
 
-export function handlePrefetch({ store, currentRoute }) {
+export function handlePrefetch({ store, currentRoute, redirect }) {
   const { slug } = currentRoute.params;
   const query = {
     category: slug,
   };
+  const categories = getters.categories.bind({ $store: store })();
+  const category = categories.find((cat) => cat.slug === slug);
+  if (!category) redirect('/404');
   return store.dispatch(FETCH_STORIES, { refresh: true, query });
 }
--- src/pages/Source/handleStore.js
+++ src/pages/Source/handleStore.js
@@ -1,6 +1,8 @@
 import { mapGetters, mapActions } from 'vuex';
 import { Getters, Actions } from '../../store/Stories/constants';
+import { Getters as SourceGetters } from '../../store/Sources/constants';
 
+// getters
 const {
   STORIES,
   LOADING,
@@ -8,12 +10,18 @@ const {
   TOTAL,
 } = Getters;
 const {
+  LIST: SOURCE_LIST,
+} = SourceGetters;
+
+// actions
+const {
   FETCH_STORIES,
 } = Actions;
 
 export const getters = {
   ...mapGetters({
     stories: STORIES,
+    sources: SOURCE_LIST,
     loading: LOADING,
     perPage: PER_PAGE,
     total: TOTAL,
@@ -26,10 +34,13 @@ export const actions = {
   }),
 };
 
-export function handlePrefetch({ store, currentRoute }) {
+export function handlePrefetch({ store, currentRoute, redirect }) {
   const { slug } = currentRoute.params;
   const query = {
     source: slug,
   };
+  const sources = getters.sources.bind({ $store: store })();
+  const source = sources.find((cat) => cat.slug === slug);
+  if (!source) redirect('/404');
   return store.dispatch(FETCH_STORIES, { refresh: true, query });
 }
