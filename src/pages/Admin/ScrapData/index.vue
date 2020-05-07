c64b6932253bd26fd5bfdfe19fb8c52bc836d0df
Karma <mlive_SKhan2@freedommobile.ca>
2020-05-07 21:28:03 +0500

    fixed scrap test and scrap data

--- src/pages/Admin/ScrapData/index.vue
+++ src/pages/Admin/ScrapData/index.vue
@@ -12,7 +12,7 @@
                     <q-icon size="1.2em" name="arrow_forward" color="purple" />
                   </template>
                   <q-breadcrumbs-el to="/admin" label="Home" icon="home" />
-                  <q-breadcrumbs-el label="scrap test" />
+                  <q-breadcrumbs-el label="scrap data" />
                 </q-breadcrumbs>
               </q-card-section>
             </q-card>
@@ -26,10 +26,10 @@
             <q-card class="main-card" flat bordered>
               <q-card-section>
                   <q-card-section class="q-pt-xs col-xs-12">
-                    <div class="text-overline">Scrap Test</div>
-                    <div class="text-h5 q-mt-sm q-mb-xs">Test Scrap Links</div>
+                    <div class="text-overline">Scrap Data</div>
+                    <div class="text-h5 q-mt-sm q-mb-xs">Scrap Links</div>
                     <div class="text-caption text-grey-8">
-                      Please select a source below, select it's link and then click on Test
+                      Please select a source below and then click on Go
                     </div>
                   </q-card-section>
                   <q-card-section>
@@ -55,38 +55,10 @@
                             </template>
                           </q-select>
                         </div>
-                        <div class="col-md-4">
-                          <q-select
-                            :disable="!form.source"
-                            outlined
-                            label="Source Link"
-                            v-model="form.link"
-                            lazy-rules
-                            :option-value="opt => opt._id"
-                            :option-label="opt => opt.url"
-                            :rules="[rules.REQUIRED]"
-                            :options="(linkList.length && linkList) || [{value: '', url: 'No Links found'}]"
-                            map-options
-                            emit-value
-                          >
-                            <template v-slot:prepend>
-                              <q-icon name="home" />
-                            </template>
-                          </q-select>
-                        </div>
-                        <div class="col-md-4">
-                          <q-input
-                            label="Number of items"
-                            outlined
-                            type="number"
-                            v-model="form.numItems"
-                            :rules="[rules.NUM_GT_0]"
-                          />
-                        </div>
                         <div class="col-md-12">
-                          <q-btn :loading="loading" :disable="loading" type="submit" outline color="green" size="16px">
+                          <q-btn :disable="!form.source" @click="openLink(`/api/v1/scrapper/source/${form.source}`)" outline color="green" size="16px">
                             <q-icon name="check"></q-icon>
-                            <div class="q-ml-xs text-center">Test</div>
+                            <div class="q-ml-xs text-center">Go</div>
                           </q-btn>
                         </div>
                       </div>
@@ -97,51 +69,6 @@
           </div>
         </div>
         <!-- add card [END] -->
-
-        <!-- stories cards [START] -->
-
-        <div class="col-12">
-          <div class="row">
-            <div
-              class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4"
-              v-for="story in stories"
-              :key="story.title"
-            >
-              <q-card class="news-card" flat bordered>
-                <q-card-section
-                  :horizontal="$q.screen.gt.xs"
-                  :vertical="$q.screen.lt.xs"
-                >
-                  <q-card-section class="q-pt-xs col-8">
-                    <div class="text-overline">{{ story.sourceName }}</div>
-                    <div
-                      @click="popup.details = story; popup.isOpen = true"
-                      class="text-h5 q-mt-sm q-mb-xs cursor-pointer"
-                      v-html="story.title"
-                    ></div>
-                    <div
-                      class="text-caption text-grey-8"
-                      v-html="story.description"
-                    ></div>
-                  </q-card-section>
-
-                  <q-card-section class="col-4 flex flex-right">
-                    <q-img
-                      class="rounded-borders"
-                      :src="story.media"
-                      height="100%"
-                    />
-                  </q-card-section>
-                </q-card-section>
-
-                <q-card-actions>
-                  <q-btn flat round icon="event" />
-                </q-card-actions>
-              </q-card>
-            </div>
-          </div>
-        </div>
-        <!-- stories cards [END] -->
       </div>
     </div>
     <q-dialog v-model="popup.isOpen">
@@ -178,8 +105,6 @@ export default {
       redirect: '',
       form: {
         source: '',
-        link: '',
-        numItems: 0,
       },
       loading: false,
       rules: { ...validations },
@@ -215,6 +140,10 @@ export default {
   },
   methods: {
     ...actions,
+    openLink(link) {
+      const routeData = this.$router.resolve(link);
+      window.open(routeData.href, '_blank');
+    },
     next() {
       const data = {
         form: this.form,
