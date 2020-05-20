906bbb6f42f29b3610b8037aa9c576a0188ca563
Karma <mlive_SKhan2@freedommobile.ca>
2020-05-20 10:57:31 +0500

    added timestamps to axios

--- src/interceptors/requestInterceptors.js
+++ src/interceptors/requestInterceptors.js
@@ -10,6 +10,9 @@ export default function (axios) {
       if (!process.env.SERVER && !reqConfig.headers.noLoader) {
         setLoading(true);
       }
+      const reqUrl = new URL(reqConfig.url);
+      reqUrl.searchParams.append('_', Date.now());
+      reqConfig.url = reqUrl.toString();
       const { token } = LocalStorage.getItem(JWT_AUTH) || {};
       if (token) reqConfig.headers.authorization = token;
       return reqConfig;
