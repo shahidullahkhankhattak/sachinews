9913336f085e87845eb22f40558436b7aa3ced6f
Karma <mlive_SKhan2@freedommobile.ca>
2020-05-25 18:39:51 +0500

    added sitemap

--- src-ssr/server/index.js
+++ src-ssr/server/index.js
@@ -4,6 +4,8 @@ const socketio = require('socket.io');
 const cors = require('./cors');
 const apiRoutes = require('./api/routes');
 const bindSocketEvents = require('./sockets');
+const SitemapController = require('./controllers/sitemapController');
+const SitemapValidator = require('./validators/sitemapValidator');
 
 const port = process.env.PORT || 8081;
 
@@ -25,4 +27,5 @@ module.exports = function ({ app }) {
   }
   app.use(cors);
   app.use('/api/v1', apiRoutes);
+  app.get('/:lang/sitemap.xml', [SitemapValidator.validate], SitemapController.index);
 };
