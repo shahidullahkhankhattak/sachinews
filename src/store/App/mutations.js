eec719cb54ed3aee8aa56913cd1ddb55a05095f2
Karma <mlive_SKhan2@freedommobile.ca>
2020-06-01 13:40:28 +0500

    added country value

--- src/store/App/mutations.js
+++ src/store/App/mutations.js
@@ -1,9 +1,9 @@
 export function setLocale({ config }, payload) {
-  config.locale = { ...payload };
+  config.locale = payload && { ...payload };
 }
 
 export function setCountry({ config }, payload) {
-  config.country = { ...payload };
+  config.country = payload && { ...payload };
 }
 export function setCounts(state, payload) {
   state.counts = payload;
